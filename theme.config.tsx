977af9b28fa795981ad1f4147ffc219733e2e96d
Opod <48951218+DylanL0ng@users.noreply.github.com>
2024-04-18 20:30:39 +0100

    init docs

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>PCRP Core</span>,
+  project: {
+    link: 'https://github.com/helix-game/rp-core/tree/main',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Hypersonic Laboratories @ 2024',
+  },
+}
+
+export default config
